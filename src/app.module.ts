8d45261092324f0a910c7423fecead863dd4037d
Andrew Lobban <andrewglobban@gmail.com>
2019-11-08 15:25:52 -0600

    Add MongoDB via Mongoose

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,19 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
+
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [
+    MongooseModule.forRoot(
+      'mongodb+srv://druwslugz:' + process.env.MONGO_ATLAS_PW + '@udemy-mean-course-gi57r.mongodb.net/zeta-data',
+      {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+      },
+    ),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
